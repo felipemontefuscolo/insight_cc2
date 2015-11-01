87bbec671a07098fe316ffd6323fb0df039aab6b
Felipe Montefuscolo <felipe.mt87@gmail.com>
2015-11-01 18:09:23 -0500

    second

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,7 +1,65 @@
-import json  # to read json data
+import json      # for reading json data
+import bisect    # for the sorted list
+from collections import defaultdict
+from pprint import pprint
+
+tweets = ['']
+with open('../tweet_output/ft1.txt') as txt:
+    tweets = txt.read().splitlines()
+
+# Function to remove an element from a sorted list
+def removeL(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bisect.bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        del a[i]
+    #raise ValueError
+
+# This class implementation was inspired by http://stackoverflow.com/questions/19472530/representing-graphs-data-structure-in-python
+class Graph(object):
+    """ Graph data structure, undirected by default. """
+
+    def __init__(self, connections):
+        self._graph = defaultdict(list)
+        self.add_connections(connections)
+
+    def add_connections(self, connections):
+        """ Add connections (list of tuple pairs) to graph """
+
+        for node1, node2 in connections:
+            self.add(node1, node2)
+
+    def add(self, node1, node2):
+        """ Add connection between node1 and node2 """
+
+        bisect.insort(self._graph[node1], node2)
+        bisect.insort(self._graph[node2], node1)
+
+    def removeEdge(self, node1, node2):
+        removeL(self._graph[node1], node2)
+        removeL(self._graph[node2], node1)
+        #if node2 in self._graph[node1]: self._graph[node1].remove(node2)
+        #if node1 in self._graph[node2]: self._graph[node2].remove(node1)
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
+        #return '{}({})'.format('merda', 'merda')
+        #return 'merda'
+
+    def p(self):
+        pprint(dict(self._graph))
+
+connections = [('A', 'B'), ('B', 'C'), ('B', 'D'),('C', 'D'), ('E', 'F'), ('F', 'C')]
+g = Graph(connections)
+g.p()
 
 
-txt = open('../tweet_input/tweets.txt').read()
+
+
+
+
+
+
 
 
 
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -8,21 +8,21 @@ def isUnicode(s):
     else:
         return False
 
+# Create a vector of tweets, i.e., each element is a tweet 
 tweets = []
 with open('../tweet_input/tweets.txt') as txt:
     tweets = txt.read().splitlines()
 
+# Output file
 ft1 = open('../tweet_output/ft1.txt', 'w')
 
 num_unicodes = 0
 for tt in tweets:
     data = json.loads(tt)
     txt = data['text']
-    #print(txt + data['created_at'] )
     if isUnicode(txt):
         num_unicodes = num_unicodes+1
         txt = txt.encode('ascii', 'ignore')
-    #print(txt + " (timestamp: " + data['created_at'] + ")" )
     ft1.write(txt + " (timestamp: " + data['created_at'] + ")\n")
 
 ft1.write('\n' + str(num_unicodes) + ' tweets contained unicode.\n')
